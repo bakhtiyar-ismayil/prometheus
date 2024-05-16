115b5d2f5e4eca5056e6b8583187657e441f81a6
bakhtiyar ismayil <bakhtiyar.ismayil@gmail.com>
2024-05-16 18:36:17 +0400

    Thu May 16 18:36:17 +04 2024

---
+++ node_exporter/node_exporter.sh
@@ -0,0 +1,30 @@
+tar -zxvf /tmp/node_exporter-1.8.0.linux-amd64.tar.gz 
+
+
+mv /tmp/node_exporter-1.8.0.linux-amd64/node_exporter /usr/local/bin/
+
+useradd --system --shell /bin/false node_exporter
+
+chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+tee /etc/systemd/system/node_exporter.service <<"EOF"
+
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo restorecon -rv /usr/local/bin/node_exporter
+
+sudo systemctl daemon-reload 
+
+sudo systemctl enable --now node_exporter
